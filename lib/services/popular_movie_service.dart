24a952c7b147621c498af62926a227cafe64bb38
Flutter-DoHyunKim <k.dh@cashwalk.io>
2023-07-21 10:07:10 +0900

    Feature 주석 추가

--- lib/services/popular_movie_service.dart
+++ lib/services/popular_movie_service.dart
@@ -15,7 +15,7 @@ class PopularMovieService{
 
   Dio dio = Dio();
   const String userToken='eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZDVlNTcyMDJhNDBlYWNmZTEzNzI1MDA3NGY4ZjFhZiIsInN1YiI6IjY0YjhhMGI4NGQyM2RkMDE0NDhjNGI3ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.oJA2FnxFAJUWqSUgWq2X0IqFsZGyrcxjWWOK8ivekc4';
-
+///해당 api key 같은 것은 어디에 숨길까? .env 파일 만들어서? flutter_dotenv import?
   try {
     final response = await dio.get("https://api.themoviedb.org/3/movie/popular",
         options: Options(contentType: 'application/json',
